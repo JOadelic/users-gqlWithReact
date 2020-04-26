c9060ffad814050a206046780fd8796321bed436
Jordan Owens <jordanowens@Jordans-MacBook-Pro.local>
2020-04-26 13:29:16 -0400

    adds rootQuery object

--- schema/schema.js
+++ schema/schema.js
@@ -1,6 +1,12 @@
 const graphql = require("graphql");
+const _ = require("lodash");
 
 const { GraphQLObjectType, GraphQLString, GraphQLInt } = graphql;
+
+const users = [
+  { id: "023", firstName: "Clark", age: 50 },
+  { id: "044", firstName: "Richard", age: 34 }
+];
 
 const UserType = new GraphQLObjectType({
   name: "User",
@@ -8,5 +14,18 @@ const UserType = new GraphQLObjectType({
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt }
+  }
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return _.find(users, { id: args.id });
+      }
+    }
   }
 });
