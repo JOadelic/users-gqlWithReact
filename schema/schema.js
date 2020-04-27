ca4e6b05c627856e7f7cd28358cbc3c773db2d86
Jordan Owens <jordanowens@Jordans-MacBook-Pro.local>
2020-04-27 10:50:02 -0400

    adds axios

--- schema/schema.js
+++ schema/schema.js
@@ -1,12 +1,6 @@
 const graphql = require("graphql");
-const _ = require("lodash");
-
+const axios = require("axios");
 const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
-
-const users = [
-  { id: "023", firstName: "Clark", age: 50 },
-  { id: "044", firstName: "Richard", age: 34 }
-];
 
 const UserType = new GraphQLObjectType({
   name: "User",
@@ -24,7 +18,9 @@ const RootQuery = new GraphQLObjectType({
       type: UserType,
       args: { id: { type: GraphQLString } },
       resolve(parentValue, args) {
-        return _.find(users, { id: args.id });
+        return axios
+          .get(`http://localhost:3000/users/${args.id}`)
+          .then(response => response.data);
       }
     }
   }
